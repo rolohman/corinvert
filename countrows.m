2cfdcf45e8cd5759b6661934158982161cd44ae0
rolohman <rolohman@gmail.com>
2019-02-13 11:41:20 +0000

    sped up countrows through indexing and removal of final test.

--- countrows.m
+++ countrows.m
@@ -1,19 +1,20 @@
-function [permbad,ratio]=countrows(id1,id2,cors,mncorl,mncor)
+function [permbad,ratio]=countrows(id1,id2,cors,mncorl,mncor,Gi0)
 [ni,nx]=size(cors);
 nd=max(id2);
 medrect=zeros(ni,nx);
 nbef=zeros(ni,nx);
 cbef=zeros(ni,nx);
 
-d=log(cors);
+d=log(cors)';
 
 
 for k=1:ni
     recid=and(id1<=id1(k),id2>=id2(k));
     befids=and(id1==id1(k),id2<=id2(k));
-    medrect(k,:)=mean(d(recid,:),'omitnan');
+
+    medrect(k,:)=mean(d(:,recid),2,'omitnan');
     nbef(k,:)=sum(befids);
-    cbef(k,:)=sum(d(befids,:)>mncorl,1,'omitnan');
+    cbef(k,:)=sum(d(:,befids)>mncorl,2,'omitnan')';
 end
 ratio=cbef./nbef;
 
@@ -22,8 +23,8 @@ permbad = and(medrect<mclow,ratio<0.85);
 permbad = or(permbad,isnan(cors));
 %now set permbad=true if any previous point on the row was true
 
-[badids,badjds]=find(permbad);
-for k=1:length(badids)
-    row=badids(k)+[id2(badids(k)):nd]-id2(badids(k));
-    permbad(row,badjds(k))=1;
-end
+% [badids,badjds]=find(permbad);
+% for k=1:length(badids)
+%     row=badids(k)+[id2(badids(k)):nd]-id2(badids(k));
+%     permbad(row,badjds(k))=1;
+% end
