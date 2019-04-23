7512dc70b80609dd7a50d517550a9659d436ac88
rolohman <rolohman@gmail.com>
2019-04-23 10:54:55 +0100

    fixed constant smoothing error

--- make_intcor_downlook.m
+++ make_intcor_downlook.m
@@ -44,8 +44,8 @@ switch windowtype
         windx=[1/(rx+1):1/(rx+1):1 (1-1/(rx+1)):-1/(rx+1):1/(rx+1)];
         windy=[1/(ry+1):1/(ry+1):1 (1-1/(ry+1)):-1/(ry+1):1/(ry+1)];
     case 1
-        windx=zeros(1,rx*2+1); windx(floor(rx/2):ceil(rx/2)+rx)=1;
-        windy=zeros(1,ry*2+1); windy(floor(ry/2):ceil(ry/2)+ry)=1;
+        windx=zeros(1,rx*2+1); windx((1:rx)+ceil(rx/2))=1;
+        windy=zeros(1,ry*2+1); windy((1:ry)+ceil(ry/2))=1;
     case 2
         windx=exp(-(-rx:rx).^2/2/(rx/2)^2);
         windy=exp(-(-ry:ry).^2/2/(ry/2)^2);
