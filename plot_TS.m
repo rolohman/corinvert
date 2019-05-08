d05b3f0a5ef9939298dce3011d08eb38f2fc7385
rolohman <rolohman@gmail.com>
2019-05-08 13:24:02 +0100

    plot more stuff

--- plot_TS.m
+++ plot_TS.m
@@ -1,4 +1,4 @@
-function [def, slope, intercept, res]=plot_TS(x,y)
+function [def, slope, intercept, res,def2]=plot_TS(x,y)
 pol  = '_VV';
 ddir = ['dates' pol '/'];
 decide_ints_stack
@@ -7,6 +7,7 @@ for i=1:nd
     dates(i).name    = files(i).name(1:8);
     dates(i).dn      = datenum(dates(i).name,'YYYYmmdd');
     dates(i).unw     = [ddir dates(i).name '_simple.unw'];
+    dates(i).fixunw     = [ddir dates(i).name '_simple_fix.unw'];
     dates(i).infill  = [ddir dates(i).name '_infill.unw'];
 end
 dn=[dates.dn];
@@ -29,10 +30,14 @@ for i=1:nd
     fid=fopen(dates(i).unw,'r');
     fseek(fid,((y-1)*newnx+x-1)*4,-1);
     def(i)=fread(fid,1,'real*4');
+    fid2=fopen(dates(i).unw,'r');
+    fseek(fid2,((y-1)*newnx+x-1)*4,-1);
+    def2(i)=fread(fid,1,'real*4');
 end
 
 synth=intercept+slope*dn;
 figure
 plot(dn,def,'.-')
 hold on
+plot(dn,def2,'.-')
 plot(dn,synth,'r')
